42252a693129cf067755f9e63a4a9d0f97ab5519
Toby Oxborrow <toby@oxborrow.net>
2017-04-04 23:11:15 +0800

    Game code and README
    Initial commit of the game code and README.

---
+++ GameOfLife.ino
@@ -0,0 +1,219 @@
+#include "LedControl.h"
+
+// "DIN" data in pin
+#define DIN_PIN 12
+// "CLK" clock pin
+#define CLK_PIN 11
+// "CS" pin
+#define CS_PIN 10
+// grid dimensions. should not be larger than 8x8
+#define MAX_Y 8
+#define MAX_X 8
+// time to wait between turns
+#define TURN_DELAY 1000
+// how many turns per game before starting a new game
+// you can also use the reset button on the board
+#define TURNS_MAX 60
+// how many turns to wait if there are no changes before starting a new game
+#define NO_CHANGES_RESET 10
+
+int TURNS = 0;      // counter for turns
+int NO_CHANGES = 0; // counter for turns without changes
+
+// game state. 0 is dead cell, 1 is live cell
+boolean grid[MAX_Y][MAX_X] = {
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0},
+};
+
+LedControl lc = LedControl(DIN_PIN, CS_PIN, CLK_PIN, 1);
+
+
+void setup() {
+  // seed random from unused analog pin
+  randomSeed(analogRead(0));
+
+  // initialise the LED matrix
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 0);
+  lc.clearDisplay(0);
+
+  reset_grid();
+  display_grid();
+
+  //Serial.begin(9600);
+  //debug_grid();
+}
+
+void loop() {  
+  delay(TURN_DELAY);
+  
+  play_gol();
+
+  TURNS++;
+
+  // reset the grid if no changes have occured recently
+  // for when the game enters a static stable state
+  if (NO_CHANGES > NO_CHANGES_RESET) {
+    reset_grid();
+  }
+  // reset the grid if the loop has been running a long time
+  // for when the game cycles between a few stable states
+  if (TURNS > TURNS_MAX) {
+    reset_grid();
+  }
+  
+  display_grid();
+}
+
+// play game of life
+void play_gol() {
+  /*
+    1. Any live cell with fewer than two neighbours dies, as if by loneliness.
+    2. Any live cell with more than three neighbours dies, as if by overcrowding.
+    3. Any live cell with two or three neighbours lives, unchanged, to the next generation.
+    4. Any dead cell with exactly three neighbours comes to life.
+    */
+
+  boolean new_grid[MAX_Y][MAX_X] = {
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+  };
+
+  for (int y = 0; y < 8; y++) {
+    for (int x = 0; x < 8; x++) {
+      int neighboughs = count_neighboughs(y, x);
+      if (grid[y][x] == 1) {
+        if ((neighboughs == 2) || (neighboughs == 3)) {
+          new_grid[y][x] = 1;
+        } else {
+          new_grid[y][x] = 0;
+        }
+      } else {
+        if (neighboughs == 3) {
+          new_grid[y][x] = 1;
+        } else {
+          new_grid[y][x] = 0;
+        }
+      }
+    }
+  }
+
+  // update the current grid from the new grid and count how many changes occured
+  int changes = 0;
+  for (int y = 0; y < MAX_Y; y++) {
+    for (int x = 0; x < MAX_X; x++) {
+      if (new_grid[y][x] != grid[y][x]) {
+        changes++;
+      }
+      grid[y][x] = new_grid[y][x];
+    }
+  }
+
+  // update global counter when no changes occured
+  if (changes == 0) {
+    NO_CHANGES++;
+  }
+}
+
+// count the number of neighbough live cells for a given cell
+int count_neighboughs(int y, int x) {
+  int count = 0;
+
+  // -- Row above us ---
+  if (y > 0) {
+    // above left
+    if (x > 0) {
+      count += grid[y-1][x-1];
+    }
+    // above
+    count += grid[y-1][x];
+    // above right
+    if ((x + 1) < 8) {
+      count += grid[y-1][x+1];
+    }
+  }
+  
+  // -- Same row -------
+  // left
+  if (x > 0) {
+    count += grid[y][x-1];
+  }
+  // right
+  if ((x + 1) < 8) {
+    count += grid[y][x+1];
+  }
+
+  // -- Row below us ---
+  if ((y + 1) < 8) {
+    // below left
+    if (x > 0) {
+      count += grid[y+1][x-1];
+    }
+    // below
+    count += grid[y+1][x];
+    // below right
+    if ((x + 1) < 8) {
+      count += grid[y+1][x+1];
+    }
+  }
+  
+  return count;
+}
+
+// reset the grid
+// we could set it all to zero then flip some bits on
+// but that leads to some predictable games I see quite frequently
+// instead, keep previous game state and flip some bits on
+void reset_grid() {
+  NO_CHANGES = 0;
+  TURNS = 0;
+  
+  for (int y = 0; y < MAX_Y; y++) {
+    for (int x = 0; x < MAX_X; x++) {
+      if (random(0, MAX_X) <= 1) {
+        grid[y][x] = 1;
+      }
+    }
+  }
+}
+
+// display the current grid to the LED matrix
+void display_grid() {
+  for (int y = 0; y < MAX_Y; y++) {
+    for (int x = 0; x < MAX_X; x++) {
+      lc.setLed(0, y, x, grid[y][x]);
+    }
+  }
+}
+
+/*
+// dump the state of the current grid to the serial connection
+void debug_grid() {
+  for (int y = 0; y < MAX_Y; y++) {
+    Serial.print("y(");
+    Serial.print(y);
+    Serial.print("): ");
+
+    for (int x = 0; x < MAX_X; x++) {
+      Serial.print(grid[y][x]);
+      Serial.print(", ");
+    }
+
+    Serial.println("");
+  }
+  Serial.println("");
+}
+*/
---
+++ README.md
@@ -0,0 +1,65 @@
+# Game of Life for Arduino
+
+## Introduction
+
+Small project to teach myself Arduino and programmable LEDs.
+
+Some code copied from my earlier Java applet version of the game.
+
+Game of life rules:
+1. Any live cell with fewer than two neighbours dies, as if by loneliness.
+2. Any live cell with more than three neighbours dies, as if by overcrowding.
+3. Any live cell with two or three neighbours lives, unchanged, to the next
+   generation.
+4. Any dead cell with exactly three neighbours comes to life.
+
+## Hardware
+
+* Arduino Uno R3
+* LED matrix board consisting of:
+** MAX7219 Serially Interfaced, 8-Digit LED Display Driver
+** 1088AS 8x8 3mm LED matrix
+* 5 male-to-female 1 wire patch cables
+* USB cable for sketch upload and power
+
+## Hardware Setup
+
+Using the patch cables, connect the Arduino to the matrix board as follows:
+
+* Arduino 5V to "VCC"
+* Arduino GND to "GND"
+* Arduino Digital Pin 10 to "CLK"
+* Arduino Digital Pin 11 to "CS"
+* Arduino Digital Pin 12 to "DIN"
+
+Note: The matrix board uses slightly different labels to what may be found in
+the MAX7219 datasheet.
+
+## Arduino IDE Setup
+
+You need to add the LedControl library to the Arduino IDE. From the Sketch
+menu, select Libraries. Select Manage Libraries. From the Library Manager type
+"ledcontrol" into the filter. Click on the LedControl library item and click
+the Install button that appears.
+
+Ensure the Arduino IDE is configured for your board model. From the Tools menu,
+select the Board submenu and select your Arduino board.
+
+Select the port for uploading the sketch to the board. From the Tools menu,
+select the Port submenu and select the appropriate port on your computer. If
+the port is not displayed, connect the USB cable to your computer and Arduino
+board and check again.
+
+## Installation
+
+To install the sketch to the Arduino board, open the IDE and click the
+upload button.
+
+## Usage
+
+When powered on the game will play by itself. It has conditions to reset itself
+if the board has not change for a short while or the game has run for a certain
+duration. That should avoid it being stuck in a single state for too long.
+
+You can click the reset button on the Arduino board to trigger a game reset
+too.
